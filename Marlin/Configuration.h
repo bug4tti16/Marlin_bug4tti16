13079444998a57daa6ec09e7bc8d35c20a0d39c9
Sungho Lim <sl4720@gmail.com>
2026-08-23 21:06:22 +0900

    monor step changes

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1360,7 +1360,7 @@
  * Override with M92 (when enabled below)
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 79.805, 80.07, 400, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.045, 80.07, 400, 100 }
 
 /**
  * Enable support for M92. Disable to save at least ~530 bytes of flash.
